000132ef8c61540233ee7f92f2f193d300244d22
johnnrodrigues <johnn_rodrigues@hotmail.com>
2022-11-03 02:29:21 -0300

    alteracoes no arquivo exemplo_01 da pasta variaveis

--- variaveis/exemplos_01.php
+++ variaveis/exemplos_01.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $nome = "Frodo";
 
 echo $nome;
@@ -7,8 +7,43 @@ echo $nome;
 var_dump($nome);
 
 $anoNascimento = 1991;
+*/
+
+/*$nome = "Gandalf";
+echo $nome;
+
+echo "<br/>";
+
+unset ($nome);
+
+echo $nome;
+*/
+
+$nome1 = "Aragorn";
+$sobrenome = "The Brave";
 
 
+$nomeCompleto = $nome1 . " " . $sobrenome;
+
+echo $nomeCompleto;
+echo "<br/>";
+
+//////////////////////////////
+$nome = "MiddleEarth";
+$site = 'middleearth.com';
+$ano = 2022;
+$salario = 5500.99;
+$bloqueado = false;
+//////////////////////////////
+$hobbits = array("Frodo", "Sam", "Merry", "Pippin");
+
+echo $hobbits[0];
+
+$horario = new DateTime();
+$arquivo = fopen("exemplo_01.php", "r");
+var_dump("arquivo");
+
+$nulo = NULL;
 
 
 ?>
